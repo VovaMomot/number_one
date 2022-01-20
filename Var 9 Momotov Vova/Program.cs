d85fa1e54b9582ed3d71b7752e127dc2c234266c
VovaMomot <momotov.volodymyr1121@vu.cdu.edu.ua>
2022-01-20 02:33:38 +0200

    Підрахунок суми чисел

--- Var 9 Momotov Vova/Program.cs
+++ Var 9 Momotov Vova/Program.cs
@@ -6,7 +6,9 @@ namespace _1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            int x = 7;
+            int y = 13;
+            Console.WriteLine("7 + 13 = {0}", x + y);
             Console.ReadKey();
         }
     }
